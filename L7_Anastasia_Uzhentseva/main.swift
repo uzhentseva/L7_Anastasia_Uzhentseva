e16737b0d672cd7956e140685757146f7340f9db
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2018-12-13 16:34:21 +0300

    1

--- L7_Anastasia_Uzhentseva/main.swift
+++ L7_Anastasia_Uzhentseva/main.swift
@@ -38,15 +38,15 @@ class Stock {
         guard let item = inventory[name] else {
             throw StockError.invalidSelection
         }
-        
+
         guard item.count > 0 else {
             throw StockError.outOfStock
         }
-        
+
         guard item.shipping == true else {
             throw StockError.shippingFalse
         }
-        
+
         var newItem = item
         newItem.count -= 1
         
@@ -58,62 +58,20 @@ class Stock {
 let stock = Stock()
 
 do {
-    let sellOne = try? stock.sellItem(itemName: "Product")
+    let sellOne = try stock.sellItem(itemName: "Product")
+    print(sellOne)
 } catch StockError.outOfStock {
     print("Position is out of stock")
+    
 } catch StockError.shippingFalse {
     print("Position is out of stock")
+    
 } catch StockError.invalidSelection {
     print("No such position in the stock")
 }
 
-print(try stock.sellItem(itemName: "Pen"))
+let sell2 = try? stock.sellItem(itemName: "Pencil")
+print(sell2)
 
-
-//enum TruckErrors: Error {
-//    case BelowZero
-//    case DivideByZero
-//}
-//
-//class TrunkCar {
-//    var time: Double
-//    var distance: Double
-//    let capacity: Double
-//    var currentLoad: Double
-//
-//    init(time: Double, distance: Double, capacity: Double, currentLoad: Double) {
-//        self.time = time
-//        self.distance = distance
-//        self.capacity = capacity
-//        self.currentLoad = currentLoad
-//    }
-//
-//    func calculateSpeed() throws -> Double {
-//        return self.distance / self.time
-//
-//        guard time > 0 else {
-//            throw TruckErrors.DivideByZero
-//        }
-//    }
-//
-//    func loadTruck (weight: Double) throws -> String {
-//        let load = self.currentLoad + weight
-//
-//        guard load > 0 else {
-//            throw TruckErrors.BelowZero
-//        }
-//    }
-//}
-//
-//extension TrunkCar : CustomStringConvertible {
-//    var description: String {
-//        return "Грузовик проехал \(self.distance) км за \(self.time) часов, скорость составила \(self.calculateSpeed()) км/ч. Назрузка составила \(self.currentLoad) из \(self.capacity) возможных.\n"
-//    }
-//}
-//
-//do {
-//    let trunkCar1 = try TrunkCar(time: 8, distance: 600, capacity: 500, currentLoad: 100)
-//}
-//
-//
-//
+let sell3 = try stock.sellItem(itemName: "Ruler")
+print(sell3)
